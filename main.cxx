f9791152bcf230064dea886fda66b0dabc275f74
OTER <maxymhammer@yandex.ru>
2015-11-04 21:58:19 +0200

    Add main

--- main.cxx
+++ main.cxx
@@ -12,6 +12,7 @@
 #include <boost/regex.hpp>
 
 #pragma GCC diagnostic pop
+
 int main()
 {
     auto settings = FSingleton<FSettings>::GetInstance();
@@ -20,5 +21,45 @@ int main()
     const auto &p = settings->Get<bool>("Pedantic");
 
     std:: cout << p;
+
+
+    std::ifstream in_file;
+    in_file.open("./../tests/preprocessor/comments.c");
+    if (!in_file.is_open())
+    {
+        std::cout << "Error: Cannot open file comments.c" << std::endl;
+        return 0;
+    }
+    if (in_file.fail())
+    {
+        std::cout << "Error occured." << std::endl;
+        return 0;
+    }
+
+    if (!in_file.seekg(0, std::ios::end))
+    {
+    }
+
+    auto file_length = in_file.tellg();
+    in_file.seekg(0, std::ios::beg);
+
+    std::string file_content;
+    file_content.reserve(file_length);
+
+    file_content.assign(std::istreambuf_iterator<char>(in_file), std::istreambuf_iterator<char>());
+
+    std::cout << file_content << std::endl;
+    in_file.close();
+
+    CommentsCleaner cleaner(file_content);
+
+    std::cout << "#####################" << std::endl;
+    std::cout << cleaner.ToString();
+
+    std::ofstream out_file("out.c");
+    out_file << cleaner.ToString();
+
+    out_file.close();
+
     return 0;
 }
