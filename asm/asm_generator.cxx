1cadc667963f3a5492053c53ff7162386d296889
OTER <maxymhammer@yandex.ru>
2015-12-14 00:41:40 +0200

    Generate func prologue, epilogue, header

--- asm/asm_generator.cxx
+++ asm/asm_generator.cxx
@@ -1,4 +1,77 @@
 //
 // Created by Maxym on 12/12/2015.
 //
+#include "asm_generator.hpp"
+#include "asm_context.hpp"
+
+//Disable warnings when use deprecated features like auto_ptr in boost.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <boost/format.hpp>
+#pragma GCC diagnostic pop
+
+
+
+decltype(AsmGenerator::asm_indent_) AsmGenerator::asm_indent_ = "    ";
+
+AsmGenerator::AsmGenerator(std::ostream& out, TreeSyntaxShared root) : out_(out)
+{
+    PutHeader();
+    auto ctx = std::make_shared<AsmContext>();
+    Generate(out, root, ctx);
+    PutEnding();
+}
+
+void AsmGenerator::Generate(std::ostream &out, TreeSyntaxShared root, AsmContextShared ctx)
+{
+
+}
+
+void AsmGenerator::PutInstruction(const std::string &instr, const std::string& args)
+{
+    this->out_ << asm_indent_ << instr;
+    std::string instr_args_indent_str;
+    instr_args_indent_str.append(' ', kMaxInstructionLength - instr.length() + kMinInstrArgsIndent);
+    this->out_ << instr_args_indent_str << args << "\n";
+}
+
+void AsmGenerator::PutHeader()
+{
+    this->out_ << asm_indent_ << ".code32\n" << asm_indent_ << ".text\n";
+}
+
+void AsmGenerator::PutEnding()
+{
+    PutFunctionDeclaration("_start");
+    PutFunctionPrologue();
+    PutInstruction("call", "main");
+    PutInstruction("mov", "%eax, %ebx");
+    PutInstruction("mov", "%1, %eax");
+    // TODO: Check return
+    PutFunctionEpilogue();
+}
+
+void AsmGenerator::PutFunctionDeclaration(const std::string &name)
+{
+    this->out_ << asm_indent_ << ".global " << name << "\n";
+    this->out_ << name << ":";
+}
+
+void AsmGenerator::PutFunctionPrologue()
+{
+    PutInstruction("pushl", "%ebp");
+    PutInstruction("mov", "%esp, %ebp");
+}
+
+void AsmGenerator::PutFunctionEpilogue()
+{
+    this->out_ << asm_indent_ << "leave\n";
+    this->out_ << asm_indent_ << "ret\n";
+}
+
+void AsmGenerator::PutLabel(const std::string &name)
+{
+    this->out_ << name << ":\n";
+}
+
 
--- asm/asm_generator.hpp
+++ asm/asm_generator.hpp
@@ -5,4 +5,34 @@
 #ifndef CCOMPILER_ASM_GENERATOR_HPP
 #define CCOMPILER_ASM_GENERATOR_HPP
 
+#include <iostream>
+#include "../tree_syntax/tree_syntax.hpp"
+#include "asm_context.hpp"
+
+class AsmGenerator
+{
+    enum
+    {
+        kMaxInstructionLength = 6,
+        kMinInstrArgsIndent = 3
+    };
+public:
+    explicit AsmGenerator(std::ostream& out, TreeSyntaxShared root);
+
+private:
+    void Generate(std::ostream& out, TreeSyntaxShared root, AsmContextShared ctx);
+    void PutHeader();
+    void PutEnding();
+    void PutFunctionDeclaration(const std::string& name);
+    void PutFunctionPrologue();
+    void PutFunctionEpilogue();
+    void PutLabel(const std::string& name);
+    void PutInstruction(const std::string& instr, const std::string& args);
+
+private:
+    // TODO: Replace with std::setfill('*') << std::setw(12)  ??
+    static const std::string asm_indent_;
+    std::ostream& out_;
+};
+
 #endif //CCOMPILER_ASM_GENERATOR_HPP
